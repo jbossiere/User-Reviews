5f8191aa60b812ea81d16987810cbdeb2a7160cf
jgb93 <jgb93@ougl-20-g.eplt.washington.edu>
2015-10-25 17:42:47 -0700

    5:42pm progress

---
+++ js/review.js
@@ -0,0 +1,15 @@
+Parse.initialize('4Erb7Ymb1EFlTkeJm8HyAteixPiOiE1BvHC35uKf',"viZvyOj4Z3s8AKNkRM2jgJpO1hao5hsLoRahxjkF");
+
+var Review = Parse.Object.extend('Review');
+
+$('form').submit(function() {
+	var review = new Review();
+	$(this).find('input').each(function() {
+		review.set($(this).attr('id'), $(this).val());
+		// console.log($(this).attr('id'))
+		// console.log($(this).val())
+		$(this).val('');
+	})
+	review.save();
+	return false;
+})
